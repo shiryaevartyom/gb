c57ac16101c5cebfa4d248b565bbd074527ed7d0
Shiryaev Artyom <9966889@gmail.com>
2022-11-30 00:27:58 +0300

    Сделаны задачи 19,23

---
+++ 2-C#/19-Palindrom/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход целое число любой разрядности и проверяет,
+// является ли оно палиндромом. Через строку решать нельзя.
+// 14212 -> нет;        12821 -> да;        23432 -> да
+
+    Console.WriteLine("Введите целое число:");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+         if ( Palindrom(n) == 0 ) Console.WriteLine($"Число {n}: палиндром");
+         else Console.WriteLine($"Число {n}: НЕ палиндром");
+
+
+//# Функция поиска количества разрядов в числе #################################################
+
+int DigitsInNumber  (int number)
+{
+return (int)Math.Log10(number)+1;
+
+}
+
+//# Функция проверки числа на палиндром ########################################################
+
+int Palindrom (int number)
+{
+
+int count = 0; // Флаг для хранения не совпадения
+         
+    for (int i = 1; i < DigitsInNumber(number)/2+1; i++)
+        if ( (number/(int)Math.Pow(10,DigitsInNumber(number)-i))%10 != (int)((number%Math.Pow(10,i))/Math.Pow(10,i-1)) ) count++;
+    
+    if (count > 0) return 1; // Не палиндром
+    else return 0;          // Палиндром
+
+}
