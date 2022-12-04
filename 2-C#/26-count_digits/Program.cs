58be387cd2475e28d03f6022c14f265333a7f325
Shiryaev Artyom <9966889@gmail.com>
2022-12-05 01:05:03 +0300

    homework 26

---
+++ 2-C#/26-count_digits/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26. - HARD необязательная Напишите программу, которая принимает на вход
+// целое или дробное число и выдаёт количество цифр в числе.
+// 452 -> 3, 82 -> 2 9,012 ->4
+
+    Console.WriteLine("Введите целое или дробное число положительное число:");
+    double n = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Число цифр в числе {n}: {searchDigitsInNumber(convertDoubleToInt(n))}");
+    
+int convertDoubleToInt (double num) //# Функция двигает запятую в числе double вправо за последнюю цифру
+{
+    double result = num;
+    while ( result-Math.Floor(result) != 0)
+    {
+        result = result * 10;
+    }
+    
+    return (int)result;
+}
+
+
+int searchDigitsInNumber (int num) //# Функция ищет количество цифр в целом числе ###################
+{
+    int digit = (int)Math.Log10(num)+1;
+    return digit;
+
+}
