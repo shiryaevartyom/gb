0ed7724e4753c4a150ea0759de6814276bd81575
Shiryaev Artyom <9966889@gmail.com>
2022-12-15 00:38:38 +0300

    41 43

---
+++ 2-C#/41-above_zero/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2; 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько целочисленных чисел Вы хотите ввести?:");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+int[] array = ReadNumbersInArray(arrayLength);
+
+Console.WriteLine("Вы ввели следущие числа:");
+PrintArray(array);
+
+Console.WriteLine($"Вы ввели {CheckAboveZero(array)} чисeл больше нуля.");
+
+void PrintArray(int[] someArray) 
+{
+    foreach (int el in someArray) // этот цикл только для просмотра и тут нет индексов
+        Console.Write($"{el} ");
+    Console.WriteLine();
+
+}
+
+int[] ReadNumbersInArray(int size) 
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите число {i+1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    return array;
+}
+
+int CheckAboveZero( int[] CheckedArray )
+{
+    int count = 0;
+
+    for (int i = 0; i < CheckedArray.Length; i++)
+    if ( CheckedArray[i] > 0) count += 1;
+
+    return count;
+
+}
---
+++ 2-C#/43-intersection_point/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Программа ищет координаты точки пересечения кривых");
+Console.WriteLine("y = k1 * x + b1; y = k2 * x + b2");
+
+Console.Write("Введите коэфициент b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите коэфициент b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите коэфициент k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите коэфициент k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($"Точка пересечения x,y: ");
+PrintArray(CalculatePoin(b1, b2, k1, k2));
+
+
+double[] CalculatePoin (double b1, double b2, double k1, double k2)
+{
+
+    double[] array = new double[2];
+    
+//    y = k1 * x + b1, y = k2 * x + b2
+//    k1 * x + b1 = k2 * x + b2
+//    k1 * x - k2 * x = b2 - b1
+//    x (k1 - k2) = b2 - b1
+//    x = (b2 - b1) / (k1 - k2)
+
+    array[0] = (b2-b1)/(k1-k2);
+    array[1] = k1 * array[0] + b1;
+    
+
+    return array;
+
+}
+
+void PrintArray(double[] someArray) 
+{
+    foreach (double el in someArray) // этот цикл только для просмотра и тут нет индексов
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
