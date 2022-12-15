9844b26e6efc29f43c5a5df9bb0a6a3f60ec537d
Artyom Shiryaev <9966889@gmail.com>
2022-12-15 10:06:05 +0300

    rename folders2

---
+++ 2-C#/hard-twod_array_mix/Program.cs
@@ -0,0 +1,62 @@
+﻿// задача 2 HARD необязательная. Считается за четыре обязательных ) то есть три этих
+// и одна с будущего семинара Сгенерировать массив случайных целых чисел размерностью
+// m*n (размерность вводим с клавиатуры) , причем чтоб количество элементов было четное.
+//Вывести на экран красивенько таблицей. Перемешать случайным образом элементы массива,
+//причем чтобы каждый элемент гарантированно и только один раз переместился на другое место
+//и выполнить перемешивание за m*n / 2 итераций. То есть если массив три на четыре,
+//то надо выполнить не более 6 итераций. И далее в конце опять вывести на экран как таблицу.
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [rows, cols];
+bool [,] ChangeIndexArray = new bool [rows, cols];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+        for (int j=0;j<array.GetLength(1);j++)
+            array[i,j] = new Random().Next(-20,21);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(1);j++)
+            Console.Write($"{array[i,j],3}\t");
+        Console.WriteLine();  
+    }
+}
+
+void ChangeRandomArray(int[,] array)
+{
+    int newI =0;
+    
+
+    for (int i=0;i<array.GetLength(0);i++)
+        {
+            for (int j=0;j<array.GetLength(1);j++)
+            if (CheckIsNewPair[i,j]) 
+            {
+                do
+                {
+                    newI = new Random().Next(0,array.GetLength(0));
+                    newJ = new Random().Next(0,array.GetLength(1));
+                }while( newI != newJ && );
+            }
+
+        }
+
+}
+
+// Возвращаем true если этот индекс еще не менялся
+bool CheckIsNewPair (int i, int j, bool[,] ChangeIndexArray)
+{
+
+    return !ChangeIndexArray[i,j];
+
+}
---
+++ 2-C#/hard-twod_array_sort/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача HARD SORT необязательная. Считается за три обязательных Задайте двумерный массив из целых чисел.
+// Количество строк и столбцов задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+// Например, задан массив: 1 4 7 2 5 9 10 3 После сортировки 1 2 3 4 5 7 9 10
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+ChangeArray(array);
+Console.WriteLine("Измененный массив, у которых оба индекса нечётные, и замените эти элементы на их квадраты:");
+PrintArray(array);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-20, 21);
+
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+
+    int min = 0;
+
+    int[] sortArray = new int[rows * cols];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            sortArray
+}
